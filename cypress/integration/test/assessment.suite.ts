2786866c272a91b329143bdbd38791f35d77eca4
michelle <michelle.transbetter@gmail.com>
2021-04-12 15:45:28 +1000

    Verify CSV file downloaded

--- cypress/integration/test/assessment.suite.ts
+++ cypress/integration/test/assessment.suite.ts
@@ -155,4 +155,57 @@ describe('intelliHR test suite', () => {
     })
 
 
+    // User Story #9
+    // No Direct Reports under Ken Phil
+
+    // User Story #10
+    // Configure downloads folder
+    const downloadsFolder = Cypress.config('downloadsFolder')
+    
+    beforeEach(deleteDownloadsFolder)
+
+    context('finds file', () => {
+
+        it('CSV file', () => {
+
+            // Visit <tenant>/auth/login, it is actually not successfuly redirected, so the baseUrl is used
+            cy.visit('/auth/login')
+
+            // Log in as a manager
+            // Type username
+            cy.get('input[name="username"]').type(environment.Admin_username)
+
+            // Type password
+            cy.get('input[name="password"]').type(environment.Admin_password)
+
+            // Find sign in button and click
+            cy.get('button[type="submit"]').click()
+
+            // Set viewport to 550px x 750px
+            cy.viewport(1366, 1024) 
+
+            // Click People
+            cy.contains('People').click()
+
+            // Wait for 10 seconds
+            cy.wait(10000)
+
+            // Click Export People to download
+            cy.contains('Export People').click()
+        
+            // Wait for 5 seconds
+            cy.wait(5000)
+
+            cy.log('**find the file**')
+            const mask = downloadsFolder
+            
+            // Validate CSV file with returned filename
+            cy.task('findFiles', mask).then((foundFile) => {
+                expect(foundFile).to.be.a('string')
+                cy.log(`found file ${foundFile}`)
+                cy.log('**confirm downloaded file**')
+                validateCsvFile(`${foundFile}`)
+            })
+        })
+    })
 })
